6dec07f443a74974f2edf9f395065918e0104ad4
Tak1Maker <salim.mouttaki@proton.me>
2023-12-19 14:57:18 +0100

    first commit

---
+++ UniqueString/Program.cs
@@ -0,0 +1,11 @@
+﻿//Program to verify by an algorithm if element contains double
+
+bool Test(string word)
+{
+    var keys = word.Split();
+    foreach (var elements in word)
+    {
+        
+    }
+    return true;
+}
